4d1586f54caca8644361973101bccf8e19f18add
rp89783 <rp89783@gmail.com>
2019-10-26 13:16:54 -0400

    Mi primer Comentario

---
+++ App8Prueba/App8Prueba/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using App8Prueba.Models;
+
+namespace App8Prueba.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
